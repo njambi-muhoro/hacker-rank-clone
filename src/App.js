27ca65491c197ffae122221d3601b1c2d51bd750
brianonyango <brian.ogango@student.moringaschool.com>
2023-04-13 12:16:20 +0300

    create authcontext

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import NavBar from "./Components/NavBar"
 import Login from "./Components/Login"
 import HomePage from "./Components/HomePage"
+import Footer from "./Components/Footer";
+import AuthProvider from "./Components/context/AuthContext";
 
 
 
@@ -12,19 +14,15 @@ function App() {
   
   return (
     <>
+    <AuthProvider>
     <NavBar/>
-    
-
     <Routes>
-
-    
       <Route path='/' element={<HomePage/>}/>
-      
-      
       <Route  path="/login" element={<Login/>} />
-      <Route path="/register" element={<SignUp/>} />
-
-    </Routes>
+      <Route path="/signup" element={<SignUp/>} />
+      </Routes>
+      <Footer />
+      </AuthProvider>
     </>
   );
 }
