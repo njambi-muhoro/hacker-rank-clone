77dd28ce66c360478248371ffca41db92c70b3c4
brianonyango <brian.ogango@student.moringaschool.com>
2023-04-17 10:55:58 +0300

    create assessment and library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import SignUp from "./Components/SignUp";
 import { Routes, Route } from "react-router";
-import "./App.css";
 import NavBar from "./Components/NavBar";
 import Login from "./Components/Login";
 import HomePage from "./Components/HomePage";
@@ -9,6 +8,8 @@ import AuthProvider from "./Components/context/AuthContext";
 import DashBoard from "./Components/DashBoard";
 import Library from "./Components/Library";
 import Assessments from "./Components/Assessments";
+// import CodeEditor from "./Components/CodeEditor";
+import Details from "./Components/Details";
 
 
 
@@ -24,7 +25,9 @@ function App() {
 					<Route path='/signup' element={<SignUp />} />
 					<Route path='/dashboard' element={<DashBoard />} />
 					<Route path="/library" element={<Library />} />
-					<Route path="/assessments" element={<Assessments/>}/>
+					<Route path="/assessments" element={<Assessments />} />
+					{/* <Route path="/codeEditor" element={<CodeEditor/>}/> */}
+					<Route path="/details/:id" element={<Details/>}/>
 				</Routes>
 				<Footer />
 			</AuthProvider>
