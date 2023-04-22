5e2419f487fe6f12c9f5a618ff15ec49df879833
brianonyango <brian.ogango@student.moringaschool.com>
2023-04-22 22:20:06 +0300

    student assessments

--- src/Components/Details/Table.jsx
+++ src/Components/Details/Table.jsx
@@ -23,7 +23,7 @@ function Table() {
   }, [id]);
 
   return (
-    <div className="flex lg:max-w-6xl mx-auto px-10 sm:px-6 lg:px-8 h-[100vh] w-full mt-20">
+    <div className="flex mt-5 sm:px-6 lg:px-8 h-[100vh] w-full">
       <div className="flex flex-col w-full">
         <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
@@ -46,31 +46,32 @@ function Table() {
                   </tr>
                 </thead>
                 <tbody>
-                  {Array.isArray(assessments.invitations) &&
-                    assessments.invitations.map(invitation => (
-                      <tr
-                        key={invitation.id}
-                        className={
-                          invitation.status === 'completed'
-                            ? 'border-b bg-neutral-100 dark:border-neutral-500 dark:bg-neutral-700'
-                            : 'border-b bg-white dark:border-neutral-500 dark:bg-neutral-600'
-                        }
-                      >
-                        <td className="whitespace-nowrap px-6 py-4 font-medium">
-                          {invitation.email}
-                        </td>
-                        <td className="whitespace-nowrap px-6 py-4">
-                          {invitation.status}
-                        </td>
-                        <td className="whitespace-nowrap px-6 py-4">
-                          {invitation.end_date}
-                        </td>
-                        <td className="whitespace-nowrap px-6 py-4">
-                          {invitation.note}
-                        </td>
-                      </tr>
-                    ))}
-                </tbody>
+  {Array.isArray(assessments?.invitations) &&
+    assessments.invitations.map(invitation => (
+      <tr
+        key={invitation.id}
+        className={
+          invitation.status === 'completed'
+            ? 'border-b bg-neutral-100 dark:border-neutral-500 dark:bg-neutral-700'
+            : 'border-b bg-white dark:border-neutral-500 dark:bg-neutral-600'
+        }
+      >
+        <td className="whitespace-nowrap px-6 py-4 font-medium">
+          {invitation.email}
+        </td>
+        <td className="whitespace-nowrap px-6 py-4">
+          {invitation.status}
+        </td>
+        <td className="whitespace-nowrap px-6 py-4">
+          {invitation.end_date}
+        </td>
+        <td className="whitespace-nowrap px-6 py-4">
+          {invitation.note}
+        </td>
+      </tr>
+    ))}
+</tbody>
+
               </table>
             </div>
           </div>
--- src/Components/ViewKata.jsx
+++ src/Components/ViewKata.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useState, useEffect } from "react";
-import React from "react"
 import { useParams } from "react-router-dom";
 
 import CodeMirror from '@uiw/react-codemirror';
